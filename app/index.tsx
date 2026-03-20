db3fdb17d1c297c858d953380a304857d2a981e3
Sh1roe-Dev <ryanjakedaz7@gmail.com>
2026-03-20 12:07:03 +0800

    myAct

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { Image, Pressable, Text, View } from "react-native";
+import LinkButton from "@/components/LinkButton";
+import Man from "../assets/images/man.png";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center gap-4">
+      <Image source={Man} className="w-48 h-48 rounded-full" />
+      <View className="flex-col items-center justify-center gap-3">
+        <View className="flex-col items-center justify-center">
+          <Text className="text-lg font-semibold">Ryan Jake G. Daz</Text>
+          <Text className="text-sm text-gray-500">Full Stack Developer</Text>
+        </View>
+        <View className="flex-col items-center justify-center">
+          <Text className="text-sm">Age: 21</Text>
+          <Text className="text-sm">
+            Address: J. Sanez St., It-ba Manito Albay
+          </Text>
+        </View>
+        <View className="flex-row justify-center gap-3">
+          <LinkButton
+            title="View My Projects"
+            url="project"
+            style="bg-black"
+          />
+          <LinkButton
+            title="View My Skills"
+            url="profile"
+            style="bg-blue-500"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ app/project.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Project = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Projects</Text>
+    </View>
+  );
+};
+
+export default Project;
